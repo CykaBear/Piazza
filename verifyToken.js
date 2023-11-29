6281af4ccc31a6e9c40fd7ef94931586d2b239f0
Elliott <elliottaryan@yahoo.co.uk>
2023-11-29 15:51:16 +0000

    Added comments

--- verifyToken.js
+++ verifyToken.js
@@ -3,16 +3,15 @@ const jsonwebtoken = require('jsonwebtoken');
 function auth(req, res, next) {
     let token = req.header('auth-token');
     if (!token) {
-        return res.status(401).send({ message: 'Access denied' });
+        return res.status(401).send({ message: 'Access denied' }); //Deny invalid tokens
     }
     if (token.startsWith('Bearer ')) {
         token = token.slice(7, token.length);
     }
-
     try {
-        const verified = jsonwebtoken.verify(token, process.env.TOKEN_SECRET);
+        const verified = jsonwebtoken.verify(token, process.env.TOKEN_SECRET); // If user token is verified then add it to the request.user value
         req.user = verified; 
-        next();
+        next(); 
     } catch (err) {
         return res.status(401).send({ message: 'Token is invalid' });
     }
